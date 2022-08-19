c46f9bc0d09518454a9c0c97c25054ce32c6fa6e
Tiago das Graças Arrais <37849109+tiagogarrais@users.noreply.github.com>
2022-08-19 11:49:55 -0300

    Update [ambiente].js
    Incluindo captura de erro e alguns dados adicionais.

--- pages/ambientes/[ambiente].js
+++ pages/ambientes/[ambiente].js
@@ -26,6 +26,23 @@ export default function Ambientes(request, response) {
         document.getElementById('servidorResponsavel').innerText =
           dadosJson.servidorResponsavel
       }
+      if (dadosJson.responsabilidadeLimpeza != null) {
+        document.getElementById('responsabilidadeLimpeza').innerText =
+          dadosJson.responsabilidadeLimpeza
+      }
+      if (dadosJson.LarguraLesteOeste != null) {
+        document.getElementById('LarguraLesteOeste').innerText =
+          dadosJson.LarguraLesteOeste
+      }
+      if (dadosJson.comprimento != null) {
+        document.getElementById('comprimento').innerText = dadosJson.comprimento
+      }
+      if (dadosJson.altura != null) {
+        document.getElementById('altura').innerText = dadosJson.altura
+      }
+    })
+    .catch(function (e) {
+      console.log('Erro capturado, consulte o código para mais informações')
     })
 
   function buscarSala(event) {
@@ -70,8 +87,27 @@ export default function Ambientes(request, response) {
         <main>
           <h1>{tituloPagina}</h1>
           <h2 id="nomeAmbiente"></h2>
-          <p id="servidorResponsavel"></p>
           <div>Chave número: {ambiente}</div>
+
+          <label>
+            Servidor Responsavel: <span id="servidorResponsavel"></span>
+          </label>
+          <br />
+
+          <label>
+            Largura Leste-Oeste: <span id="LarguraLesteOeste"></span>
+          </label>
+          <br />
+
+          <label>
+            Cumprimento: <span id="comprimento"></span>
+          </label>
+          <br />
+
+          <label>
+            Servente de Limpeza: <span id="responsabilidadeLimpeza"></span>
+          </label>
+          <br />
 
           <form className="form" onSubmit={buscarSala}>
             <input
